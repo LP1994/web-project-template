dbb0a59555912e58b8dac000b0db9582094796e4
林鹏 <1227839175@qq.com>
2022-06-09 00:30:23 +0800

    日常提交。

---
+++ src/type_script_demo001/Demo001.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+function Fun1(): void{
+  console.log( 'Fun1' );
+}
+
+export {
+  Fun1,
+};
+
+export default Fun1;
---
+++ src/type_script_demo002/Demo002.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+function Fun2(): void{
+  console.log( 'Fun2' );
+}
+
+export {
+  Fun2,
+};
+
+export default Fun2;
