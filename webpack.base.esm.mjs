27ff397385eec23b70bfcfe1cbce18d56ee479d8
林鹏 <1227839175@qq.com>
2022-06-15 01:55:32 +0800

    日常提交。

--- webpack.base.esm.mjs
+++ webpack.base.esm.mjs
@@ -55,9 +55,9 @@ function Get__filename( import_meta_url = import.meta.url ){
  */
 const __dirname = Get__dirname( import.meta.url ),
   /**
-   * isPro的值为true时表示生成环境，反之开发环境。<br />
+   * isProduction的值为true时表示生成环境，反之开发环境。<br />
    */
-  isPro = ( argv => {
+  isProduction = ( argv => {
     const num1 = argv.findIndex( c => c === '--mode' );
 
     if( num1 !== -1 ){
@@ -102,10 +102,12 @@ const aliasConfig = {
     'element-plus_css': 'element-plus/dist/index.css',
     swiper_css: 'swiper/swiper-bundle.min.css',
   },
+  devServerConfig = {},
   entryConfig = {},
   /**
    * 在webpack 5中引入了实验选项，以使用户能够激活和试用实验性功能。<br />
    * 1、由于实验性功能具有宽松的语义版本控制并且可能包含重大更改，因此请确保将webpack的版本修复为次要版本，例如webpack: ~5.4.3而不是webpack: ^5.4.3或者在使用实验时使用锁定文件。<br />
+   * 2、禁用所有实验性功能：experiments: false。<br />
    */
   experimentsConfig = {
     /**
@@ -116,8 +118,9 @@ const aliasConfig = {
     /**
      * 为许多webpack 4 API启用带有弃用警告的向后兼容层。<br />
      */
-    backCompat: true, /**
-     * 启用后，webpack可以构建以http(s):协议开头的远程资源。<br />
+    backCompat: true,
+    /**
+     * 开始可用版本：5.49.0+，启用后，webpack可以构建以http(s):协议开头的远程资源。<br />
      * 1、启用后的使用例子：import pMap1 from 'https://cdn.skypack.dev/p-map';<br />
      * 2、除了设置成Boolean值，还可以是更加详细的Object值：<br />
      * {<br />
@@ -139,9 +142,6 @@ const aliasConfig = {
      * upgrade：boolean，检测远程资源的更改并自动升级它们。<br />
      * 1、设置后，将获取现有lockfile条目的资源，并在资源内容发生更改时升级条目。<br />
      * }<br />
-     */
-    /**
-     * buildHttp: true,
      */
     buildHttp: {
       allowedUris: [
@@ -169,9 +169,70 @@ const aliasConfig = {
      * 启用模块和块层。<br />
      */
     layers: true,
-
-    lazyCompilation: true,
-
+    /**
+     * 设置成false即可，生产环境不要设置成true，会报错！这个貌似是给开发环境用的！仅在使用“entrypoints”和“动态import”时才编译它们。它可用于Web或Node.js。<br />
+     * 1、除了设置成Boolean值，还可以是更加详细的Object值：<br />
+     * 2、开始可用版本：5.17.0+。<br />
+     * {<br />
+     * backend：自定义后端。<br />
+     * 1、有效值有3中：<br />
+     *   (1)、Function<br />
+     *   ( compiler: Compiler, callback: (err?: Error, api?: BackendApi) => void ) => void
+     *   参数说明：<br />
+     *   Compiler：import('../lib/Compiler')
+     *   BackendApi：import('../lib/hmr/LazyCompilationPlugin').BackendApi
+     *   
+     *   (2)、Function<br />
+     *   (compiler: Compiler) => Promise<BackendApi>
+     *   参数说明：<br />
+     *   Compiler：import('../lib/Compiler')
+     *   BackendApi：import('../lib/hmr/LazyCompilationPlugin').BackendApi
+     *   
+     *   (3)、Object<br />
+     *   {<br />
+     *   client：string，自定义客户端。<br />
+     *   
+     *   listen：指定从服务器收听的位置。<br />
+     *   1、有效值类型有3种：<br />
+     *     1)number：表示一个端口。<br />
+     *     
+     *     2)ListenOptions：import('net').ListenOptions
+     *     参数说明：<br />
+     *     {<br />
+     *     host：string，表示一个主机。<br />
+     *     port：number，表示一个端口。<br />
+     *     }<br />
+     *     
+     *     3)Function：(server: Server) => void
+     *     参数说明：<br />
+     *     Server：import('net').Server
+     *   
+     *   protocol：指定客户端用于连接服务器的协议。<br />
+     *   1、有效值："http"、"https"。<br />
+     *   
+     *   server：指定如何创建处理EventSource请求的服务器。<br />
+     *   1、有效值类型有3种：<br />
+     *     1)ServerOptionsImport：import('http').ServerOptions
+     *     
+     *     2)ServerOptionsHttps：import('https').ServerOptions
+     *     
+     *     3)Function：() => Server
+     *     参数说明：<br />
+     *     Server：import('net').Server
+     *   }<br />
+     * 
+     * entries：boolean，为“entries”启用延迟编译。<br />
+     * 
+     * imports：boolean，为“动态import”启用延迟编译。<br />
+     * 1、开始可用版本：5.20.0+。<br />
+     * 
+     * test：string、RegExp、( module: import( '../lib/Module' ) ) => boolean，指定应该延迟编译哪些导入的模块。<br />
+     * 1、开始可用版本：5.20.0+。<br />
+     * 2、指定应延迟编译哪些“entrypoints”或“动态import”。这与导入的模块匹配，而不是入口点名称。<br />
+     * 3、当使用函数作为该项的值时，函数参数“module”的数据类型为“Module”，值形如：import( '../lib/Module' )。<br />
+     * }<br />
+     */
+    lazyCompilation: false,
     /**
      * 一旦启用，webpack将尽可能输出ECMAScript模块语法。例如，import()加载块，ESM导出以公开块数据等等。<br />
      */
@@ -185,6 +246,89 @@ const aliasConfig = {
      * 支持Top Level Await Stage 3提案，当在顶层使用await时，它使模块成为异步模块。当experiments.futureDefaults设置为true时默认启用。<br />
      */
     topLevelAwait: true,
+  },
+  /**
+   * externals配置选项提供了一种从输出包中排除依赖项的方法。此功能通常对库开发人员最有用，但它有多种应用程序。<br />
+   * 1、防止捆绑某些导入的包，而是在运行时检索这些外部依赖项。<br />
+   * 2、有效值类型：string、object、function、RegExp、[ string ]、[ object ]、[ function ]、[ RegExp ]。<br />
+   * 3、使用说明及其例子：<br />
+   * 假定webpack.config.js中externals选项有这样的配置：<br />
+   * externals: {
+   *   jquery: '$',
+   *   或这样写
+   *   jquery: 'window.$',
+   *   或这样写
+   *   jquery: 'jQuery',
+   *   或这样写，先假定用这个写法
+   *   jquery: 'window.jQuery',
+   * }
+   *
+   * 假定index.js有这样的使用jquery：<br />
+   * import $ from 'jquery';
+   * $( 'h1' ).text( 'hello world!' );
+   *
+   * 假定index.html有这样的引入CDN上的jquery：<br />
+   * <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+   *
+   * 以上的示例，最终的结果就是原本要被打包进代码里的jquery包代码系不会被打包进结果里的，而“index.js”中的$( 'h1' )被转为window.jQuery( 'h1' )。<br />
+   * webpack.config.js中externals配置里的key（如：jquery）对应index.js中from关键字右边的字符串'jquery'，这两个一一对应的单词都要一样哦，区分大小写的。<br />
+   * index.js中from关键字左边的$，当在后续的代码中使用这个$时，会被替换成webpack.config.js中externals配置里key（如：jquery）对应的value值（如：window.jQuery）。<br />
+   * 为什么可以在window上能点到这个jQuery呢，因为，在index.html中引入了jquery，且它有在全局暴露出jQuery、$这两个变量名，所以不仅可以使用window.jQuery，还可以使用window.$、jQuery、$。<br />
+   *
+   * 要想externals选项的设置生效，得满足3步设置：<br />
+   * 第1步得在externals选项中有所设置；<br />
+   * 第2步得在代码中import了第1步中设置的包名；<br />
+   * 第3步得在全局环境中存在一个可以供全局都能访问到的变量名，可以是在HTML中引入外部链接脚本，也可以是在代码中明确写了挂载在window上的全局变量，例如：window.xxx = () => {}。<br />
+   */
+  externalsConfig = {
+    // echarts: 'window.echarts',
+  },
+  /**
+   * 以下选项表示是否填充或模拟某些Node.js的全局变量。<br />
+   * 1、此功能由webpack内部的NodeStuffPlugin插件提供。<br />
+   * 2、从webpack 5开始，只能在node选项下配置global、__filename、__dirname。<br />
+   * 3、如果正在寻找如何在webpack 5下的Node.js中以类似方式填充fs等等node模块，请查看resolve.fallback以获取帮助。<br />
+   * 4、从webpack 3.0.0开始，可以将node选项直接设置为false以完全关闭NodeStuffPlugin插件。<br />
+   */
+  nodeConfig = {
+    global: false,
+    __filename: false,
+    __dirname: false,
+  },
+  /**
+   * 这些选项允许您控制webpack如何通知您超出特定文件限制的资产和入口点。此功能的灵感来自webpack性能预算的想法。<br />
+   * 1、配置性能提示的显示方式。例如，如果您的资产超过250kb，webpack将发出警告通知您。<br />
+   * 2、设置为false时，直接关闭该功能。<br />
+   */
+  performanceConfig = {
+    assetFilter( assetFilename ){
+      const boo1 = /\.(jng|bmp|dcx|gif|icns|ico|jbig2|jpe|jpeg|jpg|pam|pbm|pcx|pgm|png|pnm|ppm|psd|rgbe|tga|tif|tiff|wbmp|xbm|xpm|svg|svgz|webp|heif|heic)$/i.test( assetFilename ),
+        boo2 = /\.(eot|otf|fon|font|ttf|ttc|woff|woff2)$/i.test( assetFilename ),
+        boo3 = /\.(js|ts|tsx|mjs|mts|cjs|cts|css|less|scss|sass|styl|stylus|pcss|postcss|html|htm|ejs|vue|json|json5|xml|txt|toml|yaml|csv|wasm|graphql|graphqls|gql)$/i.test( assetFilename );
+
+      return boo1 || boo2 || boo3;
+    },
+    /**
+     * 打开或关闭提示。此外，告诉webpack在找到提示时抛出错误或警告。<br />
+     * 1、有效值类型：string、boolean。<br />
+     * 2、当值类型为boolean时，只能设置为false，它表示不显示提示警告或错误。<br />
+     * 3、当值类型为string时，有2个：'warning'、'error'。<br />
+     * warning：将显示一条警告，通知您有大型资产。我们建议在“开发环境”中使用类似的东西。<br />
+     * error：将显示一个错误，通知您有大型资产。我们建议在“生产构建”期间使用hints: "error"，以帮助防止部署太大的生产包，从而影响网页性能。<br />
+     */
+    hints: isProduction
+           ? 'error'
+           : 'warning',
+    /**
+     * 资产是从webpack发出的任何文件。此选项控制webpack何时根据单个资产大小（以bytes为单位）发出性能提示。<br />
+     * 1、5 * 1024 * 1024 = 5242880，设置成5MB。<br />
+     */
+    maxAssetSize: 10485760,
+    /**
+     * 入口点表示将在特定条目的初始加载时间期间使用的所有资产。此选项控制webpack何时根据最大入口点大小（以bytes为单位）发出性能提示。<br />
+     * 1、5 * 1024 * 1024 = 5242880，设置成5MB。<br />
+     */
+    maxEntrypointSize: 10485760,
   },
   /**
    * 自动加载模块，而不必在任何地方“import”或“require”它们。<br />
@@ -250,23 +394,71 @@ const aliasConfig = {
   targetConfig = [
     'web',
     'es2022',
-  ];
+  ],
+  watchIgnoredArr = [
+    resolve( __dirname, './.git/' ),
+    resolve( __dirname, './.idea/' ),
+    resolve( __dirname, './assist_tools/' ),
+    resolve( __dirname, './backups/' ),
+    resolve( __dirname, './bats/' ),
+    resolve( __dirname, './configures/' ),
+    resolve( __dirname, './dist/' ),
+    resolve( __dirname, './node_modules/' ),
+    resolve( __dirname, './notes/' ),
+    resolve( __dirname, './read_me/' ),
+    resolve( __dirname, './simulation_servers/' ),
+    resolve( __dirname, './test/' ),
+    resolve( __dirname, './ts_compiled/' ),
+    resolve( __dirname, './webpack_location/' ),
+  ],
+  /**
+   * 一组用于自定义手表模式的选项。<br />
+   * 1、值类型为object，结构如下：
+   * {<br />
+   * aggregateTimeout：number，第1个文件更改后，在重建之前添加延迟。这允许webpack将在此时间段内所做的任何其他更改聚合到一个重建中。以“毫秒”为单位传递一个值。<br />
+   * 
+   * followSymlinks：boolean，在查找文件时遵循符号链接。这通常不需要，因为webpack已经使用resolve.symlinks解析了符号链接。<br />
+   * 
+   * ignored：RegExp、string、[ string ]，对于某些系统，查看许多文件可能会导致大量CPU或内存使用。可以使用正则表达式排除像node_modules这样的巨大文件夹。<br />
+   * 1、如果你使用require.context，webpack会监视你的整个目录。您将需要忽略文件和/或目录，以便不需要的更改不会触发重建。<br />
+   * 
+   * poll：boolean、number，通过传递true或指定以“毫秒”为单位的轮询间隔来打开轮询。<br />
+   * 1、如果观看对您不起作用，请尝试此选项。这可能有助于解决NFS和VirtualBox、WSL、Containers或Docker中的机器问题。<br />
+   * 2、在这些情况下，请使用轮询间隔并忽略/node_modules/等大型文件夹，以将CPU使用率降至最低。<br />
+   * 3、poll: 500，表示每500毫秒检查一次更改。<br />
+   * 
+   * stdin：boolean，当标准输入流结束时停止watching。<br />
+   * 1、stdin: false，表示当标准输入流结束时不停止watching。<br />
+   * }<br />
+   */
+    // ignored选项的例子：ignored: /node_modules/、'**/node_modules'、[ '**/files/**/*.js', '**/node_modules' ]、[ path.posix.resolve(__dirname, './ignored-dir') ]
+  watchOptionsConfig = {
+    aggregateTimeout: 500,
+    followSymlinks: false,
+    ignored: watchIgnoredArr,
+  };
 
 export {
   Get__dirname,
   Get__filename,
 
   __dirname,
-  isPro,
+  isProduction,
   isSPA,
   node_env,
 
   aliasConfig,
+  devServerConfig,
   entryConfig,
   experimentsConfig,
+  externalsConfig,
+  nodeConfig,
+  performanceConfig,
   providePluginConfig,
   recordsPathConfig,
   targetConfig,
+  watchIgnoredArr,
+  watchOptionsConfig,
 };
 
 export default {
@@ -274,14 +466,20 @@ export default {
   Get__filename,
 
   __dirname,
-  isPro,
+  isProduction,
   isSPA,
   node_env,
 
   aliasConfig,
+  devServerConfig,
   entryConfig,
   experimentsConfig,
+  externalsConfig,
+  nodeConfig,
+  performanceConfig,
   providePluginConfig,
   recordsPathConfig,
   targetConfig,
+  watchIgnoredArr,
+  watchOptionsConfig,
 };
