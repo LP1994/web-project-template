842975f4862f174aba104abe1836c8e00bf71e4b
林鹏 <1227839175@qq.com>
2022-11-02 13:09:19 +0800

    日常提交。

---
+++ test/测试图片压缩/HelloWorld.js
@@ -0,0 +1,14 @@
+/**
+ * Project: web-project-template
+ * FileDirPath: src/pages/hello_world/HelloWorld.js
+ * Author: 12278
+ * Email: 1227839175@qq.com
+ * IDE: WebStorm
+ * CreateDate: 2022-01-01 00:00:00 星期六
+ */
+
+'use strict';
+
+import svg003 from 'imgDir/3.svg';
+
+document.querySelector( 'h1' ).textContent = `Hello World!svg003--->${ svg003 }`;
