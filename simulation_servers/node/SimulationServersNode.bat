5aebe9728dc30e8ff31352a0d122992aaaa5b75a
林鹏 <1227839175@qq.com>
2022-11-10 15:35:00 +0800

    日常提交。

--- simulation_servers/node/SimulationServersNode.bat
+++
@@ -1,1 +0,0 @@
-nodemon --config nodemon.json --experimental-network-imports --experimental-import-meta-resolve --experimental-wasm-modules --experimental-top-level-await --experimental-vm-modules --experimental-json-modules --experimental-shadow-realm --trace-warnings --trace-deprecation --max-old-space-size=8192 src/App.mjs&pause
