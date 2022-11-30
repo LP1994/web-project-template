e62d5f60f3bec16d51005dc1936f118161d1937f
林鹏 <1227839175@qq.com>
2022-11-30 16:10:57 +0800

    日常提交。

--- webpack.test.esm.mjs
+++ webpack.test.esm.mjs
@@ -31,6 +31,28 @@ import ForkTsCheckerNotifierWebpackPlugin from 'fork-ts-checker-notifier-webpack
 
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
+/**
+ * 供插件“typedoc-webpack-plugin”使用的配置参数。<br />
+ * 说明：<br />
+ * 1、该插件用于为使用TypeScript编写的代码生成其对应的文档，底层是基于“typedoc”的。<br />
+ * 2、目前只建议该插件用于生产、测试环境下为指定的用TypeScript编写的文件生成其文档。<br />
+ * 3、不要用于开发环境，因为在开发环境下在生成文档后会出现不停的出现webpack重编译的情况，而且，开发环境下也不需要实时生成文档，毕竟生成文档的过程也是耗时的。<br />
+ * 4、关于使用该插件时需要的配置文件及其相关的，可参见“src/tools/ts/universal_tools/type_doc”文件夹下的“doc”文件夹、“README.md”文件、"typedoc.json"文件、“typedoc_tsconfig.json”文件，参照它们为其他需要生成文档的文件配置文件。<br />
+ * 5、当前基于的“typedoc”版本为“0.23.21”。<br />
+ * 6、如果需要为多个文件生成文档，只要配置多个诸如“{options: string}”这样的配置即可，所以变量“typedocWebpackPluginConfig”才是一个数组。<br />
+ *
+ * PS：<br />
+ * 1、插件“typedoc-webpack-plugin”由于很久不更新了，依赖的“typedoc”版本也已经发生了很大的变动，所以个人直接修改了其源码，使其能配合webpack使用，修改后的代码见“backups/typedoc-webpack-plugin/index.js”。<br />
+ *
+ * 配置参数说明：<br />
+ * 1、一般只要配置参数“options”即可，它表示“typedoc”要使用的“typedoc.json”配置文件路径，里面的写法可参见“src/tools/ts/universal_tools/type_doc”文件夹下的"typedoc.json"文件及其他相关的配置文件。<br />
+ * 2、当然也支持其他参数，详细见：<br />
+ * https://typedoc.org/schema.json <br />
+ * https://typedoc.org/guides/options/ <br />
+ * 3、“typedoc”的官方文档见：<br />
+ * https://typedoc.org/guides/overview/ <br />
+ * 4、参数“options”是描述配置文件的路径，所以要使用绝对路径（其他参数如果也是表示路径什么的，也要如此），如：options: resolve( __dirname, './src/tools/ts/universal_tools/type_doc/typedoc.json' )。<br />
+ */
 import TypedocWebpackPlugin from 'typedoc-webpack-plugin';
 
 import {
@@ -71,7 +93,29 @@ import {
   providePluginConfig,
   recordsPathConfig,
   targetConfig,
-  typedocWebpackPluginConfig,
+  /**
+   * 供插件“typedoc-webpack-plugin”使用的配置参数。<br />
+   * 说明：<br />
+   * 1、该插件用于为使用TypeScript编写的代码生成其对应的文档，底层是基于“typedoc”的。<br />
+   * 2、目前只建议该插件用于生产、测试环境下为指定的用TypeScript编写的文件生成其文档。<br />
+   * 3、不要用于开发环境，因为在开发环境下在生成文档后会出现不停的出现webpack重编译的情况，而且，开发环境下也不需要实时生成文档，毕竟生成文档的过程也是耗时的。<br />
+   * 4、关于使用该插件时需要的配置文件及其相关的，可参见“src/tools/ts/universal_tools/type_doc”文件夹下的“doc”文件夹、“README.md”文件、"typedoc.json"文件、“typedoc_tsconfig.json”文件，参照它们为其他需要生成文档的文件配置文件。<br />
+   * 5、当前基于的“typedoc”版本为“0.23.21”。<br />
+   * 6、如果需要为多个文件生成文档，只要配置多个诸如“{options: string}”这样的配置即可，所以变量“typedocWebpackPluginConfig”才是一个数组。<br />
+   *
+   * PS：<br />
+   * 1、插件“typedoc-webpack-plugin”由于很久不更新了，依赖的“typedoc”版本也已经发生了很大的变动，所以个人直接修改了其源码，使其能配合webpack使用，修改后的代码见“backups/typedoc-webpack-plugin/index.js”。<br />
+   *
+   * 配置参数说明：<br />
+   * 1、一般只要配置参数“options”即可，它表示“typedoc”要使用的“typedoc.json”配置文件路径，里面的写法可参见“src/tools/ts/universal_tools/type_doc”文件夹下的"typedoc.json"文件及其他相关的配置文件。<br />
+   * 2、当然也支持其他参数，详细见：<br />
+   * https://typedoc.org/schema.json <br />
+   * https://typedoc.org/guides/options/ <br />
+   * 3、“typedoc”的官方文档见：<br />
+   * https://typedoc.org/guides/overview/ <br />
+   * 4、参数“options”是描述配置文件的路径，所以要使用绝对路径（其他参数如果也是表示路径什么的，也要如此），如：options: resolve( __dirname, './src/tools/ts/universal_tools/type_doc/typedoc.json' )。<br />
+   */
+    typedocWebpackPluginConfig,
 } from './webpack.base.esm.mjs';
 
 export default {
@@ -245,6 +289,28 @@ export default {
     ...prefetchPluginConfig,
     new webpack.ProvidePlugin( providePluginConfig ),
 
+    /**
+     * 供插件“typedoc-webpack-plugin”使用的配置参数。<br />
+     * 说明：<br />
+     * 1、该插件用于为使用TypeScript编写的代码生成其对应的文档，底层是基于“typedoc”的。<br />
+     * 2、目前只建议该插件用于生产、测试环境下为指定的用TypeScript编写的文件生成其文档。<br />
+     * 3、不要用于开发环境，因为在开发环境下在生成文档后会出现不停的出现webpack重编译的情况，而且，开发环境下也不需要实时生成文档，毕竟生成文档的过程也是耗时的。<br />
+     * 4、关于使用该插件时需要的配置文件及其相关的，可参见“src/tools/ts/universal_tools/type_doc”文件夹下的“doc”文件夹、“README.md”文件、"typedoc.json"文件、“typedoc_tsconfig.json”文件，参照它们为其他需要生成文档的文件配置文件。<br />
+     * 5、当前基于的“typedoc”版本为“0.23.21”。<br />
+     * 6、如果需要为多个文件生成文档，只要配置多个诸如“{options: string}”这样的配置即可，所以变量“typedocWebpackPluginConfig”才是一个数组。<br />
+     *
+     * PS：<br />
+     * 1、插件“typedoc-webpack-plugin”由于很久不更新了，依赖的“typedoc”版本也已经发生了很大的变动，所以个人直接修改了其源码，使其能配合webpack使用，修改后的代码见“backups/typedoc-webpack-plugin/index.js”。<br />
+     *
+     * 配置参数说明：<br />
+     * 1、一般只要配置参数“options”即可，它表示“typedoc”要使用的“typedoc.json”配置文件路径，里面的写法可参见“src/tools/ts/universal_tools/type_doc”文件夹下的"typedoc.json"文件及其他相关的配置文件。<br />
+     * 2、当然也支持其他参数，详细见：<br />
+     * https://typedoc.org/schema.json <br />
+     * https://typedoc.org/guides/options/ <br />
+     * 3、“typedoc”的官方文档见：<br />
+     * https://typedoc.org/guides/overview/ <br />
+     * 4、参数“options”是描述配置文件的路径，所以要使用绝对路径（其他参数如果也是表示路径什么的，也要如此），如：options: resolve( __dirname, './src/tools/ts/universal_tools/type_doc/typedoc.json' )。<br />
+     */
     ...( () => typedocWebpackPluginConfig.map( item => new TypedocWebpackPlugin( item ) ) )(),
   ],
   /**
