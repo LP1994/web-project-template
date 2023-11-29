0da344beb03da14ee94ecd2749176d867391c0ab
林鹏 <1227839175@qq.com>
2023-11-29 23:16:46 +0800

    log：日常更新。

--- webpack.base.esm.mjs
+++ webpack.base.esm.mjs
@@ -3159,7 +3159,7 @@ ${ JSON.stringify( req.headers, null, ' ' ) }
             else{
               throw new Error( '你需要安装该npm包：@babel/runtime-corejs3，请在项目根目录下执行该命令：npm --force install -D @babel/runtime-corejs3' );
             }
-          } )() || '7.23.4',
+          } )() || '7.23.5',
           helpers: true,
           // 切换生成器函数是否转换为使用不污染全局范围的再生器运行时。
           regenerator: true,
@@ -4638,7 +4638,6 @@ ${ JSON.stringify( req.headers, null, ' ' ) }
            * 使用corejs: { version: '3.8', proposals: true }。这将启用core-js@3.8支持的每个提案的polyfill。<br />
            */
           corejs: {
-            // 截至20231121，core-js版本为3.33.3。
             version: ( () => {
               const coreJSVersionStr = package_json.devDependencies[ 'core-js' ];
 
