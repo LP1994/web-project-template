407e0f308ae48ac5c785190e13a3679d2cfa13c4
林鹏 <1227839175@qq.com>
2023-12-30 15:11:57 +0800

    log: 日常更新。

--- webpack.base.esm.mjs
+++ webpack.base.esm.mjs
@@ -3159,7 +3159,7 @@ ${ JSON.stringify( req.headers, null, ' ' ) }
             else{
               throw new Error( '你需要安装该npm包：@babel/runtime-corejs3，请在项目根目录下执行该命令：npm --force install -D @babel/runtime-corejs3' );
             }
-          } )() || '7.23.6',
+          } )() || '7.23.7',
           helpers: true,
           // 切换生成器函数是否转换为使用不污染全局范围的再生器运行时。
           regenerator: true,
@@ -4654,7 +4654,7 @@ ${ JSON.stringify( req.headers, null, ' ' ) }
               else{
                 throw new Error( '你需要安装该npm包：core-js，请在项目根目录下执行该命令：npm --force install -D core-js' );
               }
-            } )() || '3.34.0',
+            } )() || '3.35.0',
             proposals: true,
           },
           /**
