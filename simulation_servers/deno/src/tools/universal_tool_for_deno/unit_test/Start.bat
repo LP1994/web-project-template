053e8a1cba6866af0e6b7746ea79665dc2ff7037
林鹏 <1227839175@qq.com>
2024-03-07 05:15:41 +0800

    log: 日常更新。

--- simulation_servers/deno/src/tools/universal_tool_for_deno/unit_test/Start.bat
+++ simulation_servers/deno/src/tools/universal_tool_for_deno/unit_test/Start.bat
@@ -1,1 +1,1 @@
-deno run -A --config=../../../../deno.json --lock-write --unstable --check --v8-flags=--max-old-space-size=1024000 --reload ./Main.test.mts --color=16m&pause
+deno run -A --config=../../../../deno.json --lock-write --check --v8-flags=--max-old-space-size=1024000 --reload ./Main.test.mts --color=16m&pause
--- simulation_servers/deno/test/TestStart.bat
+++ simulation_servers/deno/test/TestStart.bat
@@ -1,1 +1,1 @@
-deno run -A --watch --config=../deno.json --lock-write --unstable --check --v8-flags=--max-old-space-size=1024000 --reload ./TestStart.test.mts --color=16m&pause
+deno run -A --watch --config=../deno.json --lock-write --check --v8-flags=--max-old-space-size=1024000 --reload ./TestStart.test.mts --color=16m&pause
