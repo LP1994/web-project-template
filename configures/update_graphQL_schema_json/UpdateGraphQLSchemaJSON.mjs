c96703c9f26418fadd7edec9ec28c344a5a5e1cf
林鹏 <1227839175@qq.com>
2024-03-10 09:15:21 +0800

    log: 日常更新。

--- configures/update_graphQL_schema_json/UpdateGraphQLSchemaJSON.mjs
+++ configures/update_graphQL_schema_json/UpdateGraphQLSchemaJSON.mjs
@@ -58,8 +58,9 @@ const __dirname = Get__dirname( import.meta.url ),
 if( false ){
   // 一般形如：http://192.168.1.3:8087/graphql/schema.json
   const devURL001 = {
+    protocol: 'https:',
     host: '127.0.0.1',
-    port: '9000',
+    port: '9200',
     path: '/graphql/schema.json',
     method: 'GET',
     headers: {
@@ -140,8 +141,9 @@ if( false ){
 if( false ){
   // 一般形如：http://192.168.1.3:8087/graphql/
   const devURL001 = {
+    protocol: 'https:',
     host: '127.0.0.1',
-    port: '9000',
+    port: '9200',
     path: '/graphql/',
     method: 'POST',
     headers: {
