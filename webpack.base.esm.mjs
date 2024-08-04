cab01a77feb250a194ebd2fe447dfbcc59d8fc19
林鹏 <1227839175@qq.com>
2024-08-05 03:08:40 +0800

    log: 日常更新。

--- webpack.base.esm.mjs
+++ webpack.base.esm.mjs
@@ -4812,7 +4812,7 @@ ${ JSON.stringify( req.headers, null, ' ' ) }
                 else{
                   throw new Error( '你需要安装该npm包：core-js，请在项目根目录下执行该命令：npm --force install -D core-js' );
                 }
-              } )() || '3.37.1',
+              } )() || '3.38.0',
               proposals: true,
             },
             /**
