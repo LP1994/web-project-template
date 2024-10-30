d62a4cffb403efa9ecda263774687ffbe37200d6
林鹏 <1227839175@qq.com>
2024-10-31 01:24:45 +0800

    log: 日常更新。

--- src/pages/upload/Upload.Vue3.ts.vue
+++ src/pages/upload/Upload.Vue3.ts.vue
@@ -112,24 +112,6 @@ main {
 
   }
 
-  /*
-  > .demo001 {
-    box-sizing: content-box;
-
-    width: env(safe-area-inset-top);
-    height: env(safe-area-inset-bottom);
-
-    padding-right: env(safe-area-inset-right);
-    padding-left: env(safe-area-inset-left);
-
-    background-color: red;
-    background-clip: content-box;
-
-    margin-top: 10px;
-    margin-left: 50%;
-  }
-  */
-
 }
 </style>
 <template>
@@ -187,7 +169,6 @@ main {
         </button>
       </section>
     </article>
-<!--<div class = 'css-reset demo001'></div>-->
   </main>
 </template>
 <script
