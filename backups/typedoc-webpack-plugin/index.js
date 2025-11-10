ff0bef7588ab987dca3ca5826d93f99cf84138c3
林鹏 <1227839175@qq.com>
2025-11-10 13:39:33 +0800

    log: 日常更新。

--- backups/typedoc-webpack-plugin/index.js
+++ backups/typedoc-webpack-plugin/index.js
@@ -1,36 +1,10 @@
-/*
- *  Typedoc Webpack Plugin
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
-
-var typedoc = require( 'typedoc' );
+var fs = require( 'fs' );
 
 var clone = require( 'lodash.clone' );
 
 var merge = require( 'lodash.merge' );
 
-var fs = require( 'fs' );
+var typedoc = require( 'typedoc' );
 
 function TypedocWebpackPlugin( options ){
   if( !( options?.options ) || ( options.options.trim().length === 0 ) || !fs.statSync( options.options.trim() ).isFile() ){
@@ -44,10 +18,6 @@ resolve( __dirname, './src/tools/ts/universal_tools/type_doc/typedoc.json' )
 \n\n\n` );
   }
 
-  this.startTime = Date.now();
-
-  this.prevTimestamps = {};
-
   this.defaultTypedocOptions = {};
 
   merge( this.defaultTypedocOptions, options );
@@ -55,83 +25,42 @@ resolve( __dirname, './src/tools/ts/universal_tools/type_doc/typedoc.json' )
   this.typeDocOptions = this.defaultTypedocOptions;
 }
 
-/*
- *	@param compiler Webpack compiler object. @see <a href="https://webpack.github.io/docs/plugins.html#the-compiler-instance">label</a>
- * 	@return void
- */
 TypedocWebpackPlugin.prototype.apply = function ( compiler ){
   var self = this;
 
   compiler.hooks.emit.tapAsync( 'typedoc-webpack-plugin', async function ( compilation, callback ){
-    var arr002 = Array.from( compilation.fileSystemInfo._fileTimestamps.map.entries() )
-      .filter( function ( [ keyName, keyValue ] ){
-        return ( this.prevTimestamps?.get?.( keyName )?.safeTime || this.startTime ) < ( keyValue?.safeTime || Infinity );
-      }.bind( this ) );
+    var typedocOptions = clone( self.typeDocOptions );
 
-    var changedFiles = Array.from( new Map( arr002 ).keys() );
+    var typedocApp = await typedoc.Application.bootstrapWithPlugins( typedocOptions );
 
-    // determine if any typescript files have been changed
-    var tsFileEdited = false;
+    typedocApp.options.addReader( new typedoc.ArgumentsReader( 0 ) );
+    typedocApp.options.addReader( new typedoc.TypeDocReader() );
+    typedocApp.options.addReader( new typedoc.PackageJsonReader() );
+    typedocApp.options.addReader( new typedoc.TSConfigReader() );
+    typedocApp.options.addReader( new typedoc.ArgumentsReader( 300 ).ignoreErrors() );
 
-    for(
-      var i = 0;
-      i < changedFiles.length;
-      i++
-    ){
-      if( changedFiles[ i ].endsWith( '.ts' ) || changedFiles[ i ].endsWith( '.mts' ) || changedFiles[ i ].endsWith( '.cts' ) ){
-        tsFileEdited = true;
+    var project = await typedocApp.convert();
 
-        break;
-      }
+    if( project ){
+      typedocApp.validate( project );
     }
 
-    // if typescript files have been changed or we cannot determine what files have been changed run typedoc build
-    if( tsFileEdited || changedFiles.length === 0 ){
-      // If an absolute path set in self.typeDocOptions.out or self.typeDocOptions.json, use that
-      // else if the output path is specified in webpack config and out is relative, output typedocs relative to that path
-      var typedocOptions = clone( self.typeDocOptions );
+    if( project ){
+      var outPath = typedocApp.options.getValue( 'out' );
+      var jsonPath = typedocApp.options.getValue( 'json' );
 
-      // 使用给定的选项对象初始化 TypeDoc。
-      var typedocApp = await typedoc.Application.bootstrapWithPlugins( typedocOptions );
+      if( outPath ){
+        console.log( '\nGenerating updated typedocs.\n' );
 
-      typedocApp.options.addReader( new typedoc.ArgumentsReader( 0 ) );
-      typedocApp.options.addReader( new typedoc.TypeDocReader() );
-      typedocApp.options.addReader( new typedoc.PackageJsonReader() );
-      typedocApp.options.addReader( new typedoc.TSConfigReader() );
-      typedocApp.options.addReader( new typedoc.ArgumentsReader( 300 ).ignoreErrors() );
-
-      // 为给定的文件集运行转换器并返回生成的反射。
-      // 成功时的 ProjectReflection 实例，否则未定义。
-      var project = await typedocApp.convert();
-
-      if( project ){
-        typedocApp.validate( project );
+        await typedocApp.generateDocs( project, outPath );
       }
 
-      if( project ){
-        var outPath = typedocApp.options.getValue( 'out' );
-        var jsonPath = typedocApp.options.getValue( 'json' );
+      if( jsonPath ){
+        console.log( '\nGenerating typedoc json.\n' );
 
-        if( outPath ){
-          console.log( '\nGenerating updated typedocs.\n' );
-
-          // generateDocs(project: Models.ProjectReflection, out: string): Promise<void>
-          await typedocApp.generateDocs( project, outPath );
-        }
-
-        if( jsonPath ){
-          console.log( '\nGenerating typedoc json.\n' );
-
-          // generateJson(project: Models.ProjectReflection, out: string): Promise<void>
-          await typedocApp.generateJson( project, jsonPath );
-        }
+        await typedocApp.generateJson( project, jsonPath );
       }
     }
-    else{
-      console.warn( '\nNo ts filed changed. Not recompling typedocs.\n' );
-    }
-
-    this.prevTimestamps = compilation.fileSystemInfo._fileTimestamps.map;
 
     callback();
   } );
