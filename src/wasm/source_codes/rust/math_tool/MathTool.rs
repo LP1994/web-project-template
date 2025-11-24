d75bd0ca2456b687687ad319b98ef9bc2f6b9b07
林鹏 <1227839175@qq.com>
2025-11-24 15:09:18 +0800

    log: 日常更新。

--- src/wasm/source_codes/rust/math_tool/MathTool.rs
+++ src/wasm/source_codes/rust/math_tool/MathTool.rs
@@ -1,6 +1,54 @@
 // #[unsafe(no_mangle)] → 保证函数名不会被 Rust 编译器修改。Rust 2024语法这么要求写的！
 // extern "C" → 使用 C ABI，方便其他语言或运行时调用。
 #[unsafe(no_mangle)]
-pub extern "C" fn Add(a: i32, b: i32) -> i32 {
+pub extern "C" fn Add(a: f64, b: f64) -> f64 {
     a + b
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Sub(a: f64, b: f64) -> f64 {
+    a - b
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Mul(a: f64, b: f64) -> f64 {
+    a * b
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Div(a: f64, b: f64) -> f64 {
+    if b == 0.0 {
+        f64::NAN
+    } else {
+        a / b
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Mod(a: f64, b: f64) -> f64 {
+    if b == 0.0 {
+        f64::NAN
+    } else {
+        a % b
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Fib(index: usize, initial1: f64, initial2: f64) -> f64 {
+    if index <= 1 {
+        return initial1;
+    } else if index == 2 {
+        return initial2;
+    }
+
+    let mut prev = initial1;
+    let mut curr = initial2;
+
+    for _ in 3..=index {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    curr
 }
